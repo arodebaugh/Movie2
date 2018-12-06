5f53fccb5c1206270b88661e696277913862dd1f
ma.valerieberta <vberta20@scienceleadership.org>
2018-12-06 10:06:21 -0500

    lk

---
+++ app/src/main/java/sla/org/movie/Controller.java
@@ -0,0 +1,27 @@
+package sla.org.movie;
+
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class Controller {
+}
+
+    private ScrollView scrollDown;
+    private TextView appTextView;
+    private TextView movieTitleTextView;
+    private TextView rgrSideTextView;
+    private TextView synopsisTextView;
+
+
+
+
+
+    private Model Movie;
+
+    Controller(TextView apt, TextView mttv, TextView rstv, TextView stv, ScrollView sd) {
+        appTextView = apt;
+        movieTitleTextView = mttv;
+        rgrSideTextView = rstv;
+        synopsisTextView = stv;
+        scrollDown = sd;
+    }
